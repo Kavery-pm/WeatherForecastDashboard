2b6e85247f8d0a8498782c92c0d9cdb23ee8833e
kavery-pm <shiny.kavery@gmail.com>
2023-08-05 22:56:38 +0200

    added the layout and details design

---
+++ src/components/Layouts/sectionHeader.tsx
@@ -0,0 +1,29 @@
+import { Typography, TypographyProps } from '@mui/material';
+import React from 'react';
+
+interface SectionHeaderProps {
+  title: string;
+  mb?: string;
+}
+
+const SectionHeader: React.FC<SectionHeaderProps & TypographyProps> = ({ title, mb }) => {
+  return (
+    <Typography
+      variant="h5"
+      component="h5"
+      sx={{
+        fontSize: { xs: '12px', sm: '16px', md: '18px' },
+        color: 'rgba(255,255,255,.7)',
+        fontWeight: '600',
+        lineHeight: 1,
+        textAlign: 'center',
+        fontFamily: 'Roboto Condensed',
+        marginBottom: mb ? mb : '1rem',
+      }}
+    >
+      {title}
+    </Typography>
+  );
+};
+
+export default SectionHeader;
--- src/components/weatherToday/Details.tsx
+++ src/components/weatherToday/Details.tsx
@@ -1,5 +1,9 @@
+import Layout from "../Layouts/Layout";
+
 const Details = ()=>{
+const content = <h1>hello</h1>
     return (
-        
+     <Layout content={content} title='current Weather'/>
     )
 }
+export default Details;
