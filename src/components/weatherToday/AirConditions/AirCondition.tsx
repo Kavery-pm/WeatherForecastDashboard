937ebb5504bb6772c0bb8f683863a8b642e9e9e6
kavery-pm <shiny.kavery@gmail.com>
2023-08-06 18:02:29 +0200

    added weather condition

---
+++ src/components/weatherToday/AirConditions/AirCondition.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { WeatherData } from "../../../types";
+import AirConditionsItem from "./AirConditionsItem";
+import Layout from "../../Layouts/Layout";
+
+interface TodayWeatherAirConditionsProps {
+  data: WeatherData | null;
+}
+
+const AirConditions: React.FC<TodayWeatherAirConditionsProps> = ({
+  data,
+}) => {
+  const content = (
+    <>
+      <AirConditionsItem
+        title="Real Feel"
+        value={`${Math.round(data!.main.feels_like)} °C`}
+        type="temperature"
+      />
+      <AirConditionsItem
+        title="Wind"
+        value={`${data!.wind.speed} m/s`}
+        type="wind"
+      />
+      <AirConditionsItem
+        title="Clouds"
+        value={`${Math.round(data!.clouds.all)} %`}
+        type="clouds"
+      />
+      <AirConditionsItem
+        title="Humidity"
+        value={`${Math.round(data!.main.humidity)} %`}
+        type="humidity"
+      />
+    </>
+  );
+  return (
+    <Layout
+      title="AIR CONDITIONS"
+      content={content}
+      mb="1rem"
+      sx={{ marginTop: "2.9rem" }}
+    />
+  );
+};
+
+export default AirConditions;
--- src/components/weatherToday/Details.tsx
+++ src/components/weatherToday/Details.tsx
@@ -1,29 +1,38 @@
 import { WeatherData } from "../../types";
 import Layout from "../Layouts/Layout";
-import { Grid } from '@mui/material';
+import { Grid } from "@mui/material";
 import CityDateDetail from "./cityDateDetail";
+import { getDate } from "../../utilities/dateUtilities";
+import TemperatureDescription from "./temperatureDescription";
 interface DetailProps {
-    data: WeatherData,
-    city: string
+  data: WeatherData;
+  city: string;
 }
-const Details:React.FC<DetailProps> = ({data,city})=>{
-    const content = (
-<>
-    <Grid
-          item
-          xs={4}
-          sx={{
-            height: '80px',
-          }}
-        >
-        
-         <CityDateDetail city={city} date='10/12/2023'/>
-        </Grid>
-  
-            </>
-)
-    return (
-     <Layout content={content} title='current Weather'/>
-    )
-}
+const Details: React.FC<DetailProps> = ({ data, city }) => {
+  const currentDate = getDate(data.dt);
+
+  const content = (
+    <>
+      <Grid
+        item
+        xs={4}
+        sx={{
+          height: "80px",
+        }}
+      >
+        <CityDateDetail city={city} date={currentDate} />
+      </Grid>
+      <Grid
+        item
+        xs={4}
+        sx={{
+          height: "80px",
+        }}
+      >
+       <TemperatureDescription   temperature={data.main.temp} description={data.weather[0]?.description}/>
+      </Grid>
+    </>
+  );
+  return <Layout content={content} title="Current Weather" />;
+};
 export default Details;
