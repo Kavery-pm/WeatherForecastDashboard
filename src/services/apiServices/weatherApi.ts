4a553058e8a709d06308a4cdcae29bdf682af7ca
kavery-pm <shiny.kavery@gmail.com>
2023-08-09 11:48:21 +0200

    fixed types

--- src/services/apiServices/weatherApi.ts
+++ src/services/apiServices/weatherApi.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = '1ef47472140e1be85a3d9313547ab506';
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
-const UNITS = 'metric';
+
 
 export const fetchWeather = async (city:string) => {
   
