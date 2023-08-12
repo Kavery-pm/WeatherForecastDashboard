536e43e6fdd1cfe35d12cfcf492aa5205a4c37c5
kavery-pm <shiny.kavery@gmail.com>
2023-08-12 11:09:04 +0200

    added the error message for gelocation error

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   const [favorites, setFavorites] = useState<string[]>(getFavorites());
   const [forecast, setForecast] = useState<any>(null);
   const [geoWeather, setGeoWeather] = useState<any | null>(null);
-  const [error, seterror] = useState(null);
+  const [error, seterror] = useState<any | null>(null);
   useEffect(() => {
     // Fetch user's geolocation
     navigator.geolocation.getCurrentPosition(
@@ -32,6 +32,9 @@ function App() {
           setGeoWeather(response);
           setForecast(forecastResponse);
         } catch (error) {
+          seterror(
+            `Error fetching geolocation-based weather data:${error.message}`
+          );
           console.error(
             "Error fetching geolocation-based weather data:",
             error
@@ -39,6 +42,9 @@ function App() {
         }
       },
       (error) => {
+        seterror(
+          `Error fetching geolocation-based weather data:${error.message}`
+        );
         console.error("Error getting geolocation:", error);
       }
     );
@@ -51,10 +57,10 @@ function App() {
       setweather(cityWeatherData);
       setForecast(cityForecastData);
       seterror(null);
-    }catch(error){
-      console.log(error)
-seterror(error.message +"enter a valid city name")
-   }
+    } catch (error) {
+      console.log(error);
+      seterror(error.message + "enter a valid city name");
+    }
   };
   const handleAddFavorite = () => {
     if (cityName && weather) {
@@ -67,11 +73,9 @@ seterror(error.message +"enter a valid city name")
     }
   };
   const isCityInFavorites = cityName && favorites.includes(cityName);
- 
- 
+
   return (
     <Container maxWidth="md" className="app-container">
-       
       <Typography
         variant="h4"
         align="center"
@@ -82,115 +86,115 @@ seterror(error.message +"enter a valid city name")
       </Typography>
       {error && (
         <ErrorBox
-        margin="3rem auto"
-        flex="inherit"
-    type="error"
-    errorMessage={error}
- 
-  />
+          margin="3rem auto"
+          flex="inherit"
+          type="error"
+          errorMessage={error}
+        />
       )}
-    
-      {!error && 
-<>
-      <SearchForm onSearch={handleSearch} />
-      
-      <Grid container spacing={2} sx={{ marginTop: 2 }}>
-        <Grid item xs={12} md={4}>
-          <Paper
-            elevation={3}
-            sx={{
-              minHeight: "400px",
-              padding: 2,
-              color: "#FFFFFF",
-              background: "linear-gradient(-35deg, #000428 0%, #004e92)",
-              border: "2px solid #FFFFFF",
-              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
-              borderRadius: "8px",
-              transition: "transform 0.2s ease-in-out",
-              "&:hover": {
-                borderColor: "#00bcd4",
-                transform: "scale(1.05)",
-                cursor: "pointer",
-              },
-            }}
-          >
-            <Typography variant="h6" gutterBottom>
-              Favorite Cities
-            </Typography>
-            {favorites.length > 0 ? (
-              favorites.map((favoriteCity) => (
-                <Paper
-                  key={favoriteCity}
-                  sx={{
-                    padding: 2,
-                    marginBottom: 4,
-                    borderRadius: "4px",
-                    background:
-                      "linear-gradient(-35deg, #FF5722 0%, #FF9800 100%)",
-                    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.25)",
-                    transition: "transform 0.2s ease-in-out",
-                    fontWeight: "600",
-                    textTransform: "uppercase",
-                    "&:hover": {
-                      transform: "scale(1.05)",
-                    },
-                  }}
-                  onClick={() => handleSearch(favoriteCity)}
-                >
-                  <Typography className="favorite-city-name">
-                    {favoriteCity}
-                  </Typography>
-                </Paper>
-              ))
-            ) : (
-              <Typography>No favorite cities yet.</Typography>
-            )}
-          </Paper>
-        </Grid>
 
-        <Grid item xs={12} md={8}>
-          {geoWeather && !cityName && (
-            <Paper elevation={3} className="weather-paper">
-              <TodayWeather weather={geoWeather} city={geoWeather.name} />
-              <Box sx={{ marginTop: 2 }}></Box>
-            </Paper>
-          )}
-          {cityName && (
-            <Paper
-              elevation={3}
-              className="weather-paper"
-              sx={{ display: "block" }}
-            >
-              <TodayWeather weather={weather} city={cityName} />
-              <Box
+      {!error && (
+        <>
+          <SearchForm onSearch={handleSearch} />
+
+          <Grid container spacing={2} sx={{ marginTop: 2 }}>
+            <Grid item xs={12} md={4}>
+              <Paper
+                elevation={3}
                 sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  flexDirection: "column",
-                  marginTop: 2,
+                  minHeight: "400px",
+                  padding: 2,
+                  color: "#FFFFFF",
+                  background: "linear-gradient(-35deg, #000428 0%, #004e92)",
+                  border: "2px solid #FFFFFF",
+                  boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
+                  borderRadius: "8px",
+                  transition: "transform 0.2s ease-in-out",
+                  "&:hover": {
+                    borderColor: "#00bcd4",
+                    transform: "scale(1.05)",
+                    cursor: "pointer",
+                  },
                 }}
               >
-                {!isCityInFavorites && (
-                  <Button
-                    variant="contained"
-                    color="warning"
-                    onClick={handleAddFavorite}
+                <Typography variant="h6" gutterBottom>
+                  Favorite Cities
+                </Typography>
+                {favorites.length > 0 ? (
+                  favorites.map((favoriteCity) => (
+                    <Paper
+                      key={favoriteCity}
+                      sx={{
+                        padding: 2,
+                        marginBottom: 4,
+                        borderRadius: "4px",
+                        background:
+                          "linear-gradient(-35deg, #FF5722 0%, #FF9800 100%)",
+                        boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.25)",
+                        transition: "transform 0.2s ease-in-out",
+                        fontWeight: "600",
+                        textTransform: "uppercase",
+                        "&:hover": {
+                          transform: "scale(1.05)",
+                        },
+                      }}
+                      onClick={() => handleSearch(favoriteCity)}
+                    >
+                      <Typography className="favorite-city-name">
+                        {favoriteCity}
+                      </Typography>
+                    </Paper>
+                  ))
+                ) : (
+                  <Typography>No favorite cities yet.</Typography>
+                )}
+              </Paper>
+            </Grid>
+
+            <Grid item xs={12} md={8}>
+              {geoWeather && !cityName && (
+                <Paper elevation={3} className="weather-paper">
+                  <TodayWeather weather={geoWeather} city={geoWeather.name} />
+                  <Box sx={{ marginTop: 2 }}></Box>
+                </Paper>
+              )}
+              {cityName && (
+                <Paper
+                  elevation={3}
+                  className="weather-paper"
+                  sx={{ display: "block" }}
+                >
+                  <TodayWeather weather={weather} city={cityName} />
+                  <Box
                     sx={{
+                      display: "flex",
+                      justifyContent: "center",
                       alignItems: "center",
-                      padding: "10px",
+                      flexDirection: "column",
+                      marginTop: 2,
                     }}
                   >
-                    Add to Favorites
-                  </Button>
-                )}
-              </Box>
-            </Paper>
-          )}
-        </Grid>
-      </Grid>
-      <WeeklyForecast forecastData={forecast} />
-      </>}
+                    {!isCityInFavorites && (
+                      <Button
+                        variant="contained"
+                        color="warning"
+                        onClick={handleAddFavorite}
+                        sx={{
+                          alignItems: "center",
+                          padding: "10px",
+                        }}
+                      >
+                        Add to Favorites
+                      </Button>
+                    )}
+                  </Box>
+                </Paper>
+              )}
+            </Grid>
+          </Grid>
+          <WeeklyForecast forecastData={forecast} />
+        </>
+      )}
     </Container>
   );
 }
