2f4604df9536e559a750b6b521911da7ff062e71
kavery-pm <shiny.kavery@gmail.com>
2023-08-13 21:42:59 +0200

    added hou forecast

--- src/components/weatherToday/todayWeather.tsx
+++ src/components/weatherToday/todayWeather.tsx
@@ -1,25 +1,81 @@
-import React from "react"
-import { WeatherData } from "../../types"
-import { Card, CardContent,Box,Grid} from '@mui/material'
-import Details from "./Details"
-import AirConditions from "./AirConditions/AirCondition"
+import React from "react";
+import { ForecastItem, WeatherData } from "../../types";
+import { Card, CardContent, Box, Grid } from "@mui/material";
+import Details from "./Details";
+import AirConditions from "./AirConditions/AirCondition";
+import DayWeatherDetails from "../weeklyForecast/DayWeatherDetails";
+import Layout from "../Reusable/Layout";
+
 interface TodayWeatherProps {
-    weather: WeatherData,
-    city: string
+  weather: WeatherData;
+  city: string;
+  forecast: {
+    list: ForecastItem[];
+  };
 }
-const TodayWeather: React.FC<TodayWeatherProps> = ({weather,city})=>{
-return (
-    <Card sx={{background: 'linear-gradient(-35deg, #000428 0%, #004e92)'}}>
-    <CardContent>
-     
-      <Box mt={1}>
-      <Grid container sx={{ padding: '3rem 0rem 0rem' }} className="todayWeatherbox">
-  <Details data={weather} city={city}/>
-  <AirConditions data={weather}/>
+
+const TodayWeather: React.FC<TodayWeatherProps> = ({ weather, city, forecast }) => {
+  const currentDate = new Date().toISOString().slice(0, 10); // Get current date in YYYY-MM-DD format
+  const hourlyForecast = forecast?.list.filter((item: any) =>
+    item.dt_txt.includes(currentDate)
+  );
+
+  const hourlyContent = (
+    <Grid
+      container
+      display="flex"
+      flexDirection="column"
+     spacing={2}
+      gap="3rem"
+      sx={{ marginTop: "12px" }}
+    >
+      {hourlyForecast.map((hourlyData) => (
+        <Grid
+          item
+          key={hourlyData.dt_txt}
+          xs={2}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          sx={{
+            padding: "2px 0 2px",
+            background: "rgba(0, 0, 0, 0.05)",
+            borderRadius: "8px",
+          }}
+        >
+          <DayWeatherDetails
+            type="DayHourly"
+            day={hourlyData.dt_txt}
+            description={hourlyData.weather[0]?.description}
+          />
+        </Grid>
+      ))}
     </Grid>
-      </Box>
-    </CardContent>
-  </Card>
-)
-}
-export default TodayWeather
+  );
+
+  return (
+    <Card sx={{ background: "linear-gradient(-35deg, #000428 0%, #004e92)" }}>
+      <CardContent>
+        <Box mt={1}>
+          <Grid
+            container
+            sx={{ padding: "3rem 0rem 0rem" }}
+            className="todayWeatherbox"
+          >
+            <Details data={weather} city={city} />
+            <AirConditions data={weather} />
+            <Layout
+              title="HOURLY FORECAST"
+              content={hourlyContent}
+               mb="1rem"
+            
+               sx={{ marginTop: "2.9rem"}}
+            />
+          </Grid>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default TodayWeather;
--- src/components/weeklyForecast/DayWeatherDetails.tsx
+++ src/components/weeklyForecast/DayWeatherDetails.tsx
@@ -4,13 +4,13 @@ import { getDays } from '../../utilities/dateUtilities';
 
 interface details {
   description: string;
-  day: string,
-  type:string|undefined;
-
+  day: string;
+  type: string | undefined;
 }
-const DayWeatherDetails :React.FC<details> = ({description,day,type}) => {
-console.log(day)
-  const time = day.split(' ')[1]
+
+const DayWeatherDetails: React.FC<details> = ({ description, day, type }) => {
+  const time = day.split(' ')[1];
+
   return (
     <Grid
       container
@@ -18,52 +18,31 @@ console.log(day)
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'flex-start',
-        paddingLeft: { xs: '12px', sm: '20px', md: '32px' },
+        // paddingLeft: { xs: '12px', sm: '20px', md: '32px' },
       }}
     >
+      <Box>
         <Typography
-        variant="body1" 
-        component="div"
-        sx={{
-          fontFamily: 'Poppins',
-          fontWeight: { xs: 400, sm: 600 },
-          fontSize: { xs: '12px', sm: '13px', md: '14px' },
-        color:'white',
-          lineHeight: 1,
-          height: '31px',
-          alignItems: 'center',
-          display: 'block',
-        }}
-      >
-        {type==='hourly'?time:day}
-      
-      <Typography
-        variant="body1" 
-        component="div"
-        sx={{
-          fontFamily: 'Poppins',
-          fontWeight: { xs: 400, sm: 600 },
-          fontSize: { xs: '12px', sm: '13px', md: '14px' },
-        color:'white',
-          lineHeight: 1,
-          height: '31px',
-          alignItems: 'center',
-          display: 'block',
-        }}
-      >
-       {getDays(day)}
-      </Typography>
-      </Typography>
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'flex-start',
-          justifyContent: 'center',
-          height: '31px',
-        }}
-      >
-   
-       
+          variant="body1"
+          component="div"
+          sx={{
+            fontFamily: 'Poppins',
+            fontWeight: { xs: 400, sm: 600 },
+            fontSize: { xs: '12px', sm: '13px', md: '14px' },
+            color: 'white',
+            lineHeight: 1,
+            height: '31px',
+            alignItems: 'center',
+            display: 'block',
+          }}
+        >
+          {type === 'hourly' || type === 'DayHourly' ? time : day}
+          
+          {type === 'day' && getDays(day)}
+        </Typography>
+      </Box>
+
+      <Box>
         <Typography
           variant="h4"
           component="h4"
@@ -73,14 +52,12 @@ console.log(day)
             color: 'rgba(255,255,255, .8)',
             lineHeight: 1,
             fontFamily: 'Roboto Condensed',
-            marginLeft: 3
+           
           }}
         >
-         &nbsp; {description}
+          {description}
         </Typography>
-        
       </Box>
-       
     </Grid>
   );
 };
