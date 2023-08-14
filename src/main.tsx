a3ebeb9d74901c759f5bfb781bcd44af1e41a167
kavery-pm <shiny.kavery@gmail.com>
2023-08-14 21:39:24 +0200

    added styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-//import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App.tsx';
+import theme from './themes/themes';
+import './index.scss'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Use createRoot instead of ReactDOM.render
+const root = document.getElementById('root');
+if (root) {
+  const rootElement = createRoot(root);
+  rootElement.render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
---
+++ src/themes/themes.tsx
@@ -0,0 +1,35 @@
+import { createTheme } from "@mui/material/styles";
+import { Theme } from '@mui/material/styles';
+declare module '@mui/styles/defaultTheme' {
+   
+    interface DefaultTheme extends Theme {}
+  }
+  
+ export const defaultFontFamily = "'Poppins', sans-serif";
+export const titleFontFamily = "'Caprasimo', cursive";
+
+  
+  const theme = createTheme({
+    typography: {
+        fontFamily: defaultFontFamily,
+      },
+    palette: {
+      primary: {
+        main: "#004e92",
+      },
+      secondary: {
+        main: "#00bcd4",
+      },
+      text: {
+        primary: "#ffffff",
+      },
+      background: {
+        default: "#000428",
+      },
+    },
+  });
+  
+  export default theme;
+  
+
+
