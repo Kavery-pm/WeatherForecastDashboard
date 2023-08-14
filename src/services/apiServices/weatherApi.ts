835c1b23c538d9cd062570f1172eb6d3cd136ab3
kavery-pm <shiny.kavery@gmail.com>
2023-08-14 09:34:07 +0200

    code refactor

--- src/services/apiServices/weatherApi.ts
+++ src/services/apiServices/weatherApi.ts
@@ -4,38 +4,26 @@ const API_KEY = '1ef47472140e1be85a3d9313547ab506';
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
+async function apiRequest(url:string) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error:any) {
+    throw new Error(`Error fetching data: ${error.message}`);
+  }
+}
 
 export const fetchWeather = async (city:string) => {
-  
-    const url = `${BASE_URL}weather?q=${city}&appid=${API_KEY}`
-     
-        const response = await axios.get(url);
-        return response.data;
-    
-      }
-      export const fetchForecast = async (city:string) => {
-        const url = `${BASE_URL}forecast?q=${city}&appid=${API_KEY}`
-  
-    
-        const response = await axios.get(url);
-        return response.data;
-     
-    };
-    export async function fetchWeatherByCoordinates(latitude: number, longitude: number) {
-      const url = `${BASE_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
-      try {
-        const response = await axios.get(url
-          // `${BASE_URL}`, {
-          // params: {
-          //   lat: latitude,
-          //   lon: longitude,
-          //   appid: API_KEY, 
-          //   // or 'imperial' for Fahrenheit
-          // },
-        // }
-        );
-        return response.data;
-      } catch (error) {
-        throw new Error('Error fetching weather data by coordinates');
-      }
-    }
+  const url = `${BASE_URL}weather?q=${city}&appid=${API_KEY}`;
+  return apiRequest(url);
+}
+
+export const fetchForecast = async (city:string) => {
+  const url = `${BASE_URL}forecast?q=${city}&appid=${API_KEY}`;
+  return apiRequest(url);
+}
+
+export async function  fetchWeatherByCoordinates(latitude:number, longitude:number) {
+  const url = `${BASE_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+  return apiRequest(url);
+}
